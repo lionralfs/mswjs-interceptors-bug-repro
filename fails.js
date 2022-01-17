e501f1852188ea26f901c871cd43575942465546
Lion Ralfs <lion.ralfs@gmail.com>
2022-01-18 00:15:17 +0100

    init

---
+++ fails.js
@@ -0,0 +1,25 @@
+import { request } from 'https';
+import { createInterceptor } from '@mswjs/interceptors';
+import { interceptClientRequest } from '@mswjs/interceptors/lib/interceptors/ClientRequest/index.js';
+
+const interceptor = createInterceptor({
+  modules: [interceptClientRequest],
+  resolver(request, ref) {
+    return {
+      status: 200,
+      body: JSON.stringify({
+        success: true,
+      }),
+    };
+  },
+});
+
+interceptor.apply();
+
+interceptor.on('request', (request) => {
+  console.log('[%s] %s', request.method, request.url.toString());
+});
+
+request('https://example.com/test', (res) => {})
+  .on('error', console.error)
+  .end();
